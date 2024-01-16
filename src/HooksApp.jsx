21c6e5586d8ede69bf421abccc0800b370b0f0b7
Gomez Maximiliano <maxil5gomez@gmail.com>
2024-01-16 15:18:04 -0300

    Se creo hook useFetch

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -1,12 +1,13 @@
 // import { ContadorComponent } from "./components/ContadorComponent";
-import { FormularioComponent } from "./components/FormularioComponent";
+// import { FormularioComponent } from "./components/FormularioComponent";
+import { UsuariosComponent } from "./components/UsuariosComponent";
 
 export const HooksApp = () => {
   return (
     <>
       <h1>Aplicacion de Hooks</h1>
       <hr />
-      <FormularioComponent></FormularioComponent>
+      <UsuariosComponent></UsuariosComponent>
     </>
   );
 };
